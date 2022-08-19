d4a6fc317a6166689282cd7f0dcd599bc80bc1eb
Triblo <tricia.blosser@gmail.com>
2022-08-19 17:53:11 -0400

    add simple forms and fill out controller and table

--- app/controllers/provider_controller.rb
+++ app/controllers/provider_controller.rb
@@ -1,5 +1,40 @@
 class ProviderController < ApplicationController
   def index
-    provider_object = Provider.new(1972951945)
+    @providers = Provider.all
+  end
+
+  def new 
+    @provider = Provider.new
+  end
+
+  def create
+    require 'json'
+    require 'open-uri'
+
+    npi = strong_params[:npi]
+
+    url = "https://npiregistry.cms.hhs.gov/api/?version=2.0&number=#{npi}"
+    doc = JSON.parse(URI.open(url).read)['results'][0]
+
+    @provider = Provider.new(
+      @number = doc['basic']['number'],
+      @name = doc['basic']['name'],
+      @address = doc['basic']['addresses'][0]['address_1'],
+      @city = doc['basic']['addresses'][0]['city'],
+      @state = doc['basic']['addresses'][0]['state'],
+      @zip = doc['basic']['addresses'][0]['postal_code'],
+      @type = doc['basic']['enumeration_type'],
+      @taxonomy_code = doc['basic']['taxonomies']['code'],
+      @taxonomy_license = doc['basic']['taxonomies']['license']
+    )
+
+    @provider.save
+    redirect_to providers_path
+  end
+
+  private
+
+  def strong_params
+    params.required(:provider).permit(:npi)
   end
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,9 +1,2 @@
-class Provider
-    def initialize(npi)
-        @npi = npi
-    end
-    
-    def get_response
-        HTTPparty.get('https://npiregistry.cms.hhs.gov/api/?version=2.0&number=' + @npi)
-    end
+class Provider < ApplicationRecord
 end
